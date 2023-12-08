63442c2d0636a982bfd6eebac0b61eb8665fffa8
asfundb <muhammedbagha5@gmail.com>
2023-12-08 20:11:09 +0400

    mobile img v2

--- src/data/kitkat.js
+++ src/data/kitkat.js
@@ -1,8 +1,11 @@
 import kitkatNormalMilk250 from "../../public/kitkat-normal-milk.svg";
 import kitkatNormalMilk140 from "../../public/kitkat-140.svg";
 import kitkatWhite250 from "../../public/kitkat-normal-white.svg";
+import kitkatWhite250Mobile from "../../public/kitkat-normal-white-mobile.jpg";
 import kitkatMaxi from "../../public/kitkat-maxi-correct-test.svg";
 import kitkatNormalMilkJpg from "../../public/kitkat-normal-jpg.jpg";
+import kitkatMaxiMob from "../../public/kitkat-maxi-mobile.jpg";
+import kitkatNormalMilk250Mobile from "../../public/kitkat-normal-milk-mobile.jpg";
 
 const kitkatBall = [
   {
@@ -23,6 +26,7 @@ const kitkatBall = [
     price_a: 20,
     price_b: 25,
     img: kitkatNormalMilk250,
+    mob_img: kitkatNormalMilk250Mobile,
   },
   {
     brand: "Kitkat",
@@ -32,15 +36,17 @@ const kitkatBall = [
     price_a: 40,
     price_b: 48,
     img: kitkatMaxi,
+    mob_img: kitkatMaxiMob,
   },
   {
     brand: "Kitkat",
-    name: "White Chocolate 250 grams",
+    name: "White 250 grams",
     product_description:
       "Kitkat Ball is made of white chocolate balls (39,6%) with a crunchy cereal center (18,8%) coated with confectioner's cream (40,6%). Both crunchy and melting, these balls to share are ideal for all those who like to take a break! HAVE A BREAK, HAVE A KITKAT.",
     price_a: 20,
     price_b: 25,
     img: kitkatWhite250,
+    mob_img: kitkatWhite250Mobile,
   },
 ];
 
