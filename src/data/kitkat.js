05856e6cff762eabf1e7aa376746cad3bf1ed7d0
asfundb <muhammedbagha5@gmail.com>
2023-12-08 20:02:43 +0400

    mobile img

--- src/data/kitkat.js
+++ src/data/kitkat.js
@@ -2,7 +2,7 @@ import kitkatNormalMilk250 from "../../public/kitkat-normal-milk.svg";
 import kitkatNormalMilk140 from "../../public/kitkat-140.svg";
 import kitkatWhite250 from "../../public/kitkat-normal-white.svg";
 import kitkatMaxi from "../../public/kitkat-maxi-correct-test.svg";
-import kitkatMaxiNew from "../../public/kitkat-ball-maxi-new.svg";
+import kitkatNormalMilkJpg from "../../public/kitkat-normal-jpg.jpg";
 
 const kitkatBall = [
   {
@@ -13,6 +13,7 @@ const kitkatBall = [
     price_a: 16,
     price_b: 20,
     img: kitkatNormalMilk140,
+    mob_img: kitkatNormalMilkJpg,
   },
   {
     brand: "Kitkat",
@@ -30,7 +31,7 @@ const kitkatBall = [
       "Kitkat Ball is made of milk chocolate balls (39,6%) with a crunchy cereal center (18,8%) coated with confectioner's cream (40,6%). Both crunchy and melting, these balls to share are ideal for all those who like to take a break! HAVE A BREAK, HAVE A KITKAT.",
     price_a: 40,
     price_b: 48,
-    img: kitkatMaxiNew,
+    img: kitkatMaxi,
   },
   {
     brand: "Kitkat",
