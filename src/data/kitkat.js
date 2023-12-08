9b422ef07cf0364ac97298b73e12d691eca4ed35
asfundb <muhammedbagha5@gmail.com>
2023-12-08 23:03:42 +0400

    update price for a/b

--- src/data/kitkat.js
+++ src/data/kitkat.js
@@ -13,8 +13,8 @@ const kitkatBall = [
     name: "140 grams",
     product_description:
       "Kitkat Ball is made of milk chocolate balls (39,6%) with a crunchy cereal center (18,8%) coated with confectioner's cream (40,6%). Both crunchy and melting, these balls to share are ideal for all those who like to take a break! HAVE A BREAK, HAVE A KITKAT.",
-    price_a: 16,
-    price_b: 20,
+    price_a: 12,
+    price_b: 16,
     img: kitkatNormalMilk140,
     mob_img: kitkatNormalMilkJpg,
   },
@@ -24,7 +24,7 @@ const kitkatBall = [
     product_description:
       "Kitkat Ball is made of milk chocolate balls (39,6%) with a crunchy cereal center (18,8%) coated with confectioner's cream (40,6%). Both crunchy and melting, these balls to share are ideal for all those who like to take a break! HAVE A BREAK, HAVE A KITKAT.",
     price_a: 20,
-    price_b: 25,
+    price_b: 30,
     img: kitkatNormalMilk250,
     mob_img: kitkatNormalMilk250Mobile,
   },
@@ -33,8 +33,8 @@ const kitkatBall = [
     name: "400 grams",
     product_description:
       "Kitkat Ball is made of milk chocolate balls (39,6%) with a crunchy cereal center (18,8%) coated with confectioner's cream (40,6%). Both crunchy and melting, these balls to share are ideal for all those who like to take a break! HAVE A BREAK, HAVE A KITKAT.",
-    price_a: 40,
-    price_b: 48,
+    price_a: 35,
+    price_b: 45,
     img: kitkatMaxi,
     mob_img: kitkatMaxiMob,
   },
@@ -44,7 +44,7 @@ const kitkatBall = [
     product_description:
       "Kitkat Ball is made of white chocolate balls (39,6%) with a crunchy cereal center (18,8%) coated with confectioner's cream (40,6%). Both crunchy and melting, these balls to share are ideal for all those who like to take a break! HAVE A BREAK, HAVE A KITKAT.",
     price_a: 20,
-    price_b: 25,
+    price_b: 30,
     img: kitkatWhite250,
     mob_img: kitkatWhite250Mobile,
   },
