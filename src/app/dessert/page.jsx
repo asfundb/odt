4184ef9f5efa81d02efe859172001080dc32ac30
asfundb <muhammedbagha5@gmail.com>
2023-12-12 17:48:17 +0400

    dessert addition

---
+++ src/app/dessert/page.jsx
@@ -0,0 +1,213 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import dessert from "@/data/dessert";
+import Image from "next/image";
+import banner from "../../../public/dessert/Dessert-banner.svg";
+import { ThumbsUp, ThumbsDown } from "@phosphor-icons/react";
+import replaceSpacesWithHyphens from "@/utils/stringParser";
+import MailerLite from "@mailerlite/mailerlite-nodejs";
+import toast, { Toaster } from "react-hot-toast";
+
+const mailerlite = new MailerLite({
+  api_key: process.env.NEXT_PUBLIC_MAILER_LITE_API_KEY,
+});
+
+export default function Stouffers() {
+  const [selectedMeals, setSelectedMeals] = useState([]);
+  const [currentMeal, setCurrentMeal] = useState(null);
+  const [feedback, setFeedback] = useState({});
+  const [priceType, setPriceType] = useState("price_a");
+  const [email, setEmail] = useState("");
+
+  const updateEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const addSubscriber = (e) => {
+    e.preventDefault();
+    const params = {
+      email: email,
+      groups: ["107253465340708574"], // Dessert Group ID
+      status: "active",
+    };
+
+    mailerlite.subscribers
+      .createOrUpdate(params)
+      .then((response) => {
+        setEmail("");
+        toast.success("You're all signed up");
+      })
+      .catch((error) => {
+        if (error.response) {
+          toast.error("Try again");
+        }
+      });
+  };
+
+  useEffect(() => {
+    setPriceType(Math.random() < 0.5 ? "price_a" : "price_b");
+  }, []);
+
+  const handleFeedback = (meal, type) => {
+    setFeedback({ ...feedback, [meal.name]: type });
+  };
+
+  const addToSelectList = (index) => {
+    setSelectedMeals((prevSelected) => {
+      if (prevSelected.includes(index)) {
+        return prevSelected.filter((mealIndex) => mealIndex !== index);
+      } else {
+        return [...prevSelected, index];
+      }
+    });
+  };
+
+  const openModal = (meal, event) => {
+    event.stopPropagation();
+    setCurrentMeal(meal);
+    document.getElementById("my_modal_3").showModal();
+  };
+
+  return (
+    <div className="w-screen h-full">
+      <div className="">
+        <Image src={banner} alt="" className="w-screen " />
+      </div>
+      <div className=" mx-10 lg:mx-48 my-16 text-black flex flex-col items-center ">
+        <div className="badge bg-sky-400 border-none badge-lg lg:py-4 text-white mb-8 text-xs md:text-base mx-8 text-center py-6">
+          Select the flavours you would like to be invited to taste by clicking
+          the card
+        </div>
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+          {dessert.map((meal, index) => (
+            <div
+              key={index}
+              className={`card border w-300 cursor-pointer overflow-hidden ${
+                selectedMeals.includes(index) ? "border-green-500" : ""
+              }`}
+              onClick={() => addToSelectList(index)}
+            >
+              <Image
+                src={meal.img}
+                alt={meal.name}
+                className="hidden md:block rounded-t-2xl md:hover:scale-125 md:hover:translate-y-5 transition duration-500 cursor-pointer object-cover"
+              />
+              <Image
+                src={meal.mob_img}
+                alt={meal.name}
+                className="block md:hidden rounded-t-2xl md:hover:scale-125 md:hover:translate-y-5 transition duration-500 cursor-pointer object-cover"
+              />
+              <div className="flex justify-between items-center h-[50px] mx-4 text-xs md:text-sm">
+                <h2 className="text-center text-sm">{meal.name}</h2>
+
+                <button
+                  className="w-[25px] h-[25px] rounded-full border-black border hover:text-white hover:bg-black cursor-pointer"
+                  onClick={(e) => openModal(meal, e)}
+                  id={`D-${replaceSpacesWithHyphens(meal.name)}-more-info`}
+                >
+                  i
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="w-full items-center flex flex-col mt-20 bg-sky-100 py-20 gap-4 px-8">
+        <p className="font-bold text-black">
+          You will be invited to taste {selectedMeals.length}{" "}
+          {selectedMeals.length == 1 ? "product" : "products"}!
+        </p>
+        <form
+          action="submit"
+          className=" flex justify-center w-full"
+          onSubmit={addSubscriber}
+        >
+          <input
+            type="email"
+            className="border rounded-l-full  w-[80%] lg:w-[25%] h-[50px] indent-4 border-r-0 text-black"
+            placeholder="Enter Email"
+            onChange={updateEmail}
+            value={email}
+          />{" "}
+          <button className="btn text-white rounded-r-full border-l-0 bg-black">
+            Submit
+          </button>
+        </form>
+      </div>
+
+      {/* Modal for displaying meal details */}
+      <dialog id="my_modal_3" className="modal">
+        <div className="modal-box bg-white text-black">
+          <form method="dialog">
+            <button
+              className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+              onClick={() => setCurrentMeal(null)}
+            >
+              ✕
+            </button>
+          </form>
+          <div className="flex items-center flex-col gap-3 mt-4">
+            <Image
+              src={currentMeal?.img}
+              alt=""
+              className="hidden md:block w-[50%] rounded"
+            />
+            <Image
+              src={currentMeal?.mob_img}
+              alt=""
+              className="md:hidden w-[50%] rounded"
+            />
+            <h3 className="font-medium uppercase text-slate-600 text-sm mb-0 mt-4">
+              {currentMeal ? currentMeal.brand : ""}
+            </h3>
+            <h3 className="font-bold text-lg mt-0">
+              {currentMeal ? currentMeal.name : ""}
+            </h3>
+            {currentMeal && (
+              <>
+                <div className="badge badge-accent text-white">
+                  {currentMeal[priceType]} Dhs
+                </div>
+                <p className="py-4 text-sm border-t border-slate-100 pl-2">
+                  {currentMeal.product_description}
+                  <br />
+                </p>
+              </>
+            )}
+          </div>
+          <div className="flex justify-center flex-col items-center gap-4 border-t border-slate-100 pt-4">
+            <p className="text-sm">Is this price reasonable?</p>
+            <div className="flex justify-center gap-4">
+              <button
+                className={`border rounded-full p-4 hover:cursor-pointer ${
+                  feedback[currentMeal?.name] === "dislike"
+                    ? "bg-red-500 text-white"
+                    : ""
+                }`}
+                onClick={(e) => handleFeedback(currentMeal, "dislike")}
+                id={`D-${replaceSpacesWithHyphens(
+                  currentMeal?.name
+                )}-${priceType}-disliked`}
+              >
+                <ThumbsDown width={25} height={25} />
+              </button>
+              <button
+                className={`border rounded-full p-4 hover:cursor-pointer ${
+                  feedback[currentMeal?.name] === "like"
+                    ? "bg-green-500 text-white"
+                    : ""
+                }`}
+                onClick={(e) => handleFeedback(currentMeal, "like")}
+                id={`D-${replaceSpacesWithHyphens(
+                  currentMeal?.name
+                )}-${priceType}-liked`}
+              >
+                <ThumbsUp width={25} height={25} />
+              </button>
+            </div>
+          </div>
+        </div>
+      </dialog>
+      <Toaster position="bottom-right" reverseOrder={false} />
+    </div>
+  );
+}
---
+++ src/data/dessert.js
@@ -0,0 +1,89 @@
+import ChocolateCookieDesktop from "../../public/dessert/Desktop/Chocolate-cookies-chips.svg";
+import ChocolateCookieMobile from "../../public/dessert/Mobile/Chocolate-cookies-chips.jpg";
+import ChocolateCookieCaramelDesktop from "../../public/dessert/Desktop/Chocolate-cookie-caramel.svg";
+import ChocolateCookieCaramelMobile from "../../public/dessert/Mobile/Chocolate-cookie-caramel.jpg";
+import ChocolateBrownieDesktop from "../../public/dessert/Desktop/Chocolate-brownie.svg";
+import ChocolateBrownieMobile from "../../public/dessert/Mobile/Chocolate-brownie.jpg";
+import ChocolateFondantDesktop from "../../public/dessert/Desktop/Chocolate-fondant.svg";
+import ChocolateFondantMobile from "../../public/dessert/Mobile/Chocolate-fondant.jpg";
+import ChocolateCakeDesktop from "../../public/dessert/Desktop/Chocolate-cake.svg";
+import ChocolateCakeMobile from "../../public/dessert/Mobile/Chocolate-cake.jpg";
+import ChocolateMuffinFilledDesktop from "../../public/dessert/Desktop/Chocolate-muffin-chocolate-filled.svg";
+import ChocolateMuffinFilledMobile from "../../public/dessert/Mobile/Chocolate-muffin-chocolate-filled.jpg";
+import ChocolateMuffinChipsDesktop from "../../public/dessert/Desktop/Chocolate-muffin-chocolate-chips.svg";
+import ChocolateMuffinChipsMobile from "../../public/dessert/Mobile/Chocolate-muffin-chocolate-chips.jpg";
+
+const dessert = [
+  {
+    brand: "Nestle Dessert",
+    name: "Chocolate Cookies Preparation",
+    product_description:
+      "Indulge in the delightful simplicity of Nestle Dessert's Chocolate Cookie Mix. A specially blended mix of ingredients for effortless baking. Experience the rich, velvety texture of 100% pure cocoa butter chocolate, meticulously made from the finest cocoa beans.",
+    price_a: 0,
+    price_b: 0,
+    img: ChocolateCookieDesktop,
+    mob_img: ChocolateCookieMobile,
+  },
+  {
+    brand: "Nestle Dessert",
+    name: "Chocolate Caramel Cookie",
+    product_description:
+      "Savor the delightful combination in a Chocolate Caramel Cookie, where rich chocolate and smooth caramel blend seamlessly. Each bite offers a delicious contrast of chewy cookie and sweet, gooey caramel, creating a uniquely indulgent and memorable treat.",
+    price_a: 0,
+    price_b: 0,
+    img: ChocolateCookieCaramelDesktop,
+    mob_img: ChocolateCookieCaramelMobile,
+  },
+  {
+    brand: "Nestle Dessert",
+    name: "Chocolate Brownie",
+    product_description:
+      "Delight in the indulgent pleasure of a chocolate brownie, where its rich, fudgy texture meets deep chocolate flavor. Every bite combines a slightly crisp crust with a soft, dense interior, creating a satisfying and memorable chocolate experience.",
+    price_a: 0,
+    price_b: 0,
+    img: ChocolateBrownieDesktop,
+    mob_img: ChocolateBrownieMobile,
+  },
+  {
+    brand: "Nestle Dessert",
+    name: "Fondant Preparation",
+    product_description:
+      "Indulge in the luxurious, decadent taste of chocolate fondant, where its gooey, warm center and intensely rich chocolate flavor come together. Each bite offers a perfect balance of soft, tender cake and flowing, molten chocolate, creating an irresistibly unforgettable experience.",
+    price_a: 0,
+    price_b: 0,
+    img: ChocolateFondantDesktop,
+    mob_img: ChocolateFondantMobile,
+  },
+  {
+    brand: "Nestle Dessert",
+    name: "Chocolate Cake",
+    product_description:
+      "Savor the classic richness of chocolate cake, where every slice is a deep dive into pure chocolate bliss. This cake, with its moist, tender crumb and intense chocolate flavor, transforms a simple dessert into an exquisite experience. Each forkful offers a harmonious blend of sweetness and cocoa, making the chocolate cake a timeless, unforgettable treat.",
+    price_a: 0,
+    price_b: 0,
+    img: ChocolateCakeDesktop,
+    mob_img: ChocolateCakeMobile,
+  },
+  {
+    brand: "Nestle Dessert",
+    name: "Chocolate Muffin with Chocolate Cream",
+    product_description:
+      "Dive into the delicious chocolate filling. Made with luxurious chocolate, this smooth and rich filling turns every cake into a special treat. It's more than just a filling; it's a burst of intense chocolate flavor that makes each bite unforgettable.",
+    price_a: 0,
+    price_b: 0,
+    img: ChocolateMuffinFilledDesktop,
+    mob_img: ChocolateMuffinFilledMobile,
+  },
+  {
+    brand: "Nestle Dessert",
+    name: "Chocolate Chip Muffin",
+    product_description:
+      "Dive into the joy of chocolate chip muffins, where every bite is speckled with rich chocolate chips. These chips, melting in the warm, fluffy muffin, turn a simple treat into a delightful indulgence. With each bite, the creamy chocolate flavor bursts through, making every muffin a special, memorable delight.",
+    price_a: 0,
+    price_b: 0,
+    img: ChocolateMuffinChipsDesktop,
+    mob_img: ChocolateMuffinChipsMobile,
+  },
+];
+
+export default dessert;
