bf27ac06321cd2a8825b1fd2f51e73ca8ab93f35
asfundb <muhammedbagha5@gmail.com>
2023-12-19 17:21:53 +0400

    new cailler flavours

--- src/data/cailler.js
+++ src/data/cailler.js
@@ -28,9 +28,6 @@ import mobileCaillerMilkAlmond100 from "../../public/cailler/Mobile/Milk-chocola
 import caillerMilkAlmond200 from "../../public/cailler/Desktop/Milk-chocolate-almonds-200.svg";
 import mobileCaillerMilkAlmond200 from "../../public/cailler/Mobile/Milk-chocolate-almonds-200.jpg";
 
-// import caillerDarkAlmond100 from "../../public/cailler/Desktop/Dark-chocolate-almonds-200.svg";
-// import mobileCaillerDarkAlmond100 from "../../public/cailler/Mobile/Dark-chocolate-almonds-200.jpg";
-
 import caillerMilkHazelnut200 from "../../public/cailler/Desktop/Milk-chocolate-hazelnut-200.svg";
 import mobileCaillerMilkHazelnut200 from "../../public/cailler/Mobile/Milk-chocolate-hazelnut-200.jpg";
 
@@ -40,9 +37,14 @@ import mobileCaillerMilkHazelnut300 from "../../public/cailler/Mobile/Milk-choco
 import caillerMilkHazelnut100 from "../../public/cailler/Desktop/Milk-chocolate-hazelnut-100.svg";
 import mobileCaillerMilkHazelnut100 from "../../public/cailler/Mobile/Milk-chocolate-hazelnut-100.jpg";
 
-import caillerMilkHazelnut400 from "../../public/cailler/Desktop/Milk-chocolate-hazelnut-400.svg";
-import mobileCaillerMilkHazelnut400 from "../../public/cailler/Mobile/Milk-chocolate-hazelnut-400.jpg";
+import caillerChocmel100 from "../../public/cailler/Desktop/Milk-chocolate-chocmel-100.svg";
+import mobileCaillerChocmel100 from "../../public/cailler/Mobile/Milk-chocolate-chocmel-100.jpg";
 
+import caillerFrigor100 from "../../public/cailler/Desktop/Milk-chocolate-frigor-100.svg";
+import mobileCaillerFrigor100 from "../../public/cailler/Mobile/Milk-chocolate-frigor-100.jpg";
+
+import caillerRayon100 from "../../public/cailler/Desktop/Milk-chocolate-rayon-100.svg";
+import mobileCaillerRayon100 from "../../public/cailler/Mobile/Milk-chocolate-rayon-100.jpg";
 const cailler = [
   {
     brand: "Cailler",
@@ -116,16 +118,6 @@ const cailler = [
   },
   {
     brand: "Cailler",
-    name: "Milk Hazelnut 400g",
-    product_description:
-      "Combining fine cocoa beans and milk from local farms with whole roasted hazelnuts, this is a heavenly mix of delicate crunch and smooth Cailler chocolate.",
-    price_a: 30,
-    price_b: 20,
-    img: caillerMilkHazelnut400,
-    mob_img: mobileCaillerMilkHazelnut400,
-  },
-  {
-    brand: "Cailler",
     name: "Crunchy Caramel 100g",
     product_description:
       "The perfect combination of crunchy aerated cereals and salty caramel pieces, coated with delicious creamy Swiss Alpine milk chocolate. With its multi texture and irresistible taste, every bite is a burst of flavors that will satisfy your chocolate cravings.",
@@ -133,6 +125,36 @@ const cailler = [
     price_b: 20,
     img: caillerCrunchyCaramel100,
     mob_img: mobileCaillerCrunchyCaramel100,
+  },
+  {
+    brand: "Cailler",
+    name: "Chocmel 100g",
+    product_description:
+      "Its multi-faceted charm gives you a melt-in-your-mouth experience. A sea of ​​deliciously crunchy slivers surprises with wonderful honey and almond nuances.",
+    price_a: 30,
+    price_b: 20,
+    img: caillerChocmel100,
+    mob_img: mobileCaillerChocmel100,
+  },
+  {
+    brand: "Cailler",
+    name: "Rayon 100g",
+    product_description:
+      "From the bursting of delicate air bubbles to the splintering of honey nougat to the melting of milk chocolate. The Rayon is pleasure in crescendo.",
+    price_a: 30,
+    price_b: 20,
+    img: caillerRayon100,
+    mob_img: mobileCaillerRayon100,
+  },
+  {
+    brand: "Cailler",
+    name: "Frigor 100g",
+    product_description:
+      "There is nothing like Frigor. It has such a delicate consistency. Milk chocolate melts on the outside and a cream filling made from hazelnuts and almonds melts on the inside",
+    price_a: 30,
+    price_b: 20,
+    img: caillerFrigor100,
+    mob_img: mobileCaillerFrigor100,
   },
   {
     brand: "Cailler",
