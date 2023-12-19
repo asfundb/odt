d39488ec64065cfce4008ed3437e49aaaa5509b8
asfundb <muhammedbagha5@gmail.com>
2023-12-19 14:22:32 +0400

    cailler tablets

--- src/app/cailler/page.jsx
+++ src/app/cailler/page.jsx
@@ -1,15 +1,51 @@
 "use client";
-import React, { useState } from "react";
-import femina from "@/data/femina";
+import React, { useState, useEffect } from "react";
+import cailler from "@/data/cailler";
 import Image from "next/image";
-import banner from "../../../public/femina-banner.svg";
-import product from "../../../public/product.svg";
+import banner from "../../../public/cailler/Cailler-banner.svg";
 import { ThumbsUp, ThumbsDown } from "@phosphor-icons/react";
+import replaceSpacesWithHyphens from "@/utils/stringParser";
+import MailerLite from "@mailerlite/mailerlite-nodejs";
+import toast, { Toaster } from "react-hot-toast";
+
+const mailerlite = new MailerLite({
+  api_key: process.env.NEXT_PUBLIC_MAILER_LITE_API_KEY,
+});
 
 export default function Stouffers() {
   const [selectedMeals, setSelectedMeals] = useState([]);
   const [currentMeal, setCurrentMeal] = useState(null);
   const [feedback, setFeedback] = useState({});
+  const [priceType, setPriceType] = useState("price_a");
+  const [email, setEmail] = useState("");
+
+  const updateEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const addSubscriber = (e) => {
+    e.preventDefault();
+    const params = {
+      email: email,
+      groups: ["106907855697740829"], // KK Group ID
+      status: "active",
+    };
+
+    mailerlite.subscribers
+      .createOrUpdate(params)
+      .then((response) => {
+        setEmail("");
+        toast.success("You're all signed up");
+      })
+      .catch((error) => {
+        if (error.response) {
+          toast.error("Try again");
+        }
+      });
+  };
+
+  useEffect(() => {
+    setPriceType(Math.random() < 0.5 ? "price_a" : "price_b");
+  }, []);
 
   const handleFeedback = (meal, type) => {
     setFeedback({ ...feedback, [meal.name]: type });
@@ -36,27 +72,38 @@ export default function Stouffers() {
       <div className="">
         <Image src={banner} alt="" className="w-screen " />
       </div>
-      <div className=" mx-10 lg:mx-48 my-16 text-black flex flex-col items-center">
-        <div className="badge bg-sky-400 border-none badge-lg py-4 text-white mb-8">
+      <div className=" mx-10 lg:mx-48 my-16 text-black flex flex-col items-center ">
+        <div className="badge bg-sky-400 border-none badge-lg lg:py-4 text-white mb-8 text-xs md:text-base mx-8 text-center py-6">
           Select the flavours you would like to be invited to taste by clicking
           the card
         </div>
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-          {femina.map((meal, index) => (
+          {cailler.map((meal, index) => (
             <div
               key={index}
-              className={`card border w-300 cursor-pointer ${
+              className={`card border w-300 cursor-pointer overflow-hidden ${
                 selectedMeals.includes(index) ? "border-green-500" : ""
               }`}
               onClick={() => addToSelectList(index)}
             >
-              <Image src={product} alt={meal.name} className="rounded-t-2xl" />
-              <div className="flex justify-between items-center h-[50px] mx-4">
-                <h2 className="text-center text-sm">{meal.name}</h2>
-
+              <Image
+                src={meal.img}
+                alt={meal.name}
+                className="hidden md:block rounded-t-2xl md:hover:scale-125 md:hover:translate-y-5 transition duration-500 cursor-pointer object-cover"
+              />
+              <Image
+                src={meal.mob_img}
+                alt={meal.name}
+                className="block md:hidden rounded-t-2xl md:hover:scale-125 md:hover:translate-y-5 transition duration-500 cursor-pointer object-cover"
+              />
+              <div className="flex justify-between items-center h-[50px] mx-4 text-xs md:text-sm">
+                <h2 className="text-center text-xs  md:text-sm truncate ">
+                  {meal.name}
+                </h2>
                 <button
                   className="w-[25px] h-[25px] rounded-full border-black border hover:text-white hover:bg-black cursor-pointer"
                   onClick={(e) => openModal(meal, e)}
+                  id={`K-${replaceSpacesWithHyphens(meal.name)}-more-info`}
                 >
                   i
                 </button>
@@ -65,18 +112,24 @@ export default function Stouffers() {
           ))}
         </div>
       </div>
-      <div className="w-full items-center flex flex-col mt-20 bg-sky-100 py-20 gap-4">
+      <div className="w-full items-center flex flex-col mt-20 bg-sky-100 py-20 gap-4 px-8">
         <p className="font-bold text-black">
           You will be invited to taste {selectedMeals.length}{" "}
           {selectedMeals.length == 1 ? "product" : "products"}!
         </p>
-        <form action="submit" className=" flex justify-center w-full">
+        <form
+          action="submit"
+          className=" flex justify-center w-full"
+          onSubmit={addSubscriber}
+        >
           <input
             type="email"
-            className="border rounded-l-full  w-[80%] lg:w-[25%] h-[50px] indent-4 border-r-0"
+            className="border rounded-l-full  w-[80%] lg:w-[25%] h-[50px] indent-4 border-r-0 text-black"
             placeholder="Enter Email"
+            onChange={updateEmail}
+            value={email}
           />{" "}
-          <button className="btn text-white rounded-r-full border-l-0">
+          <button className="btn text-white rounded-r-full border-l-0 bg-black">
             Submit
           </button>
         </form>
@@ -94,7 +147,16 @@ export default function Stouffers() {
             </button>
           </form>
           <div className="flex items-center flex-col gap-3 mt-4">
-            <Image src={product} alt="" className="w-[50%] rounded" />
+            <Image
+              src={currentMeal?.img}
+              alt=""
+              className="hidden md:block w-[50%] rounded"
+            />
+            <Image
+              src={currentMeal?.mob_img}
+              alt=""
+              className="md:hidden w-[50%] rounded"
+            />
             <h3 className="font-medium uppercase text-slate-600 text-sm mb-0 mt-4">
               {currentMeal ? currentMeal.brand : ""}
             </h3>
@@ -104,7 +166,7 @@ export default function Stouffers() {
             {currentMeal && (
               <>
                 <div className="badge badge-accent text-white">
-                  {currentMeal.price_a} Dhs
+                  {currentMeal[priceType]} Dhs
                 </div>
                 <p className="py-4 text-sm border-t border-slate-100 pl-2">
                   {currentMeal.product_description}
@@ -113,30 +175,40 @@ export default function Stouffers() {
               </>
             )}
           </div>
-          <div className="flex justify-center gap-4">
-            <button
-              className={`border rounded-full p-4 hover:cursor-pointer ${
-                feedback[currentMeal?.name] === "dislike"
-                  ? "bg-red-500 text-white"
-                  : ""
-              }`}
-              onClick={() => handleFeedback(currentMeal, "dislike")}
-            >
-              <ThumbsDown width={25} height={25} />
-            </button>
-            <button
-              className={`border rounded-full p-4 hover:cursor-pointer ${
-                feedback[currentMeal?.name] === "like"
-                  ? "bg-green-500 text-white"
-                  : ""
-              }`}
-              onClick={() => handleFeedback(currentMeal, "like")}
-            >
-              <ThumbsUp width={25} height={25} />
-            </button>
+          <div className="flex justify-center flex-col items-center gap-4 border-t border-slate-100 pt-4">
+            <p className="text-sm">Is this price reasonable?</p>
+            <div className="flex justify-center gap-4">
+              <button
+                className={`border rounded-full p-4 hover:cursor-pointer ${
+                  feedback[currentMeal?.name] === "dislike"
+                    ? "bg-red-500 text-white"
+                    : ""
+                }`}
+                onClick={(e) => handleFeedback(currentMeal, "dislike")}
+                id={`K-${replaceSpacesWithHyphens(
+                  currentMeal?.name
+                )}-${priceType}-disliked`}
+              >
+                <ThumbsDown width={25} height={25} />
+              </button>
+              <button
+                className={`border rounded-full p-4 hover:cursor-pointer ${
+                  feedback[currentMeal?.name] === "like"
+                    ? "bg-green-500 text-white"
+                    : ""
+                }`}
+                onClick={(e) => handleFeedback(currentMeal, "like")}
+                id={`K-${replaceSpacesWithHyphens(
+                  currentMeal?.name
+                )}-${priceType}-liked`}
+              >
+                <ThumbsUp width={25} height={25} />
+              </button>
+            </div>
           </div>
         </div>
       </dialog>
+      <Toaster position="bottom-right" reverseOrder={false} />
     </div>
   );
 }
---
+++ src/data/cailler.js
@@ -0,0 +1,189 @@
+import caillerMilk100 from "../../public/cailler/Desktop/Milk-chocolate-100.svg";
+import mobileCaillerMilk100 from "../../public/cailler/Mobile/Milk-chocolate-100.jpg";
+
+import caillerMilk200 from "../../public/cailler/Desktop/Milk-chocolate-200.svg";
+import mobileCaillerMilk200 from "../../public/cailler/Mobile/Milk-chocolate-200.jpg";
+
+import caillerDarkHazelnut100 from "../../public/cailler/Desktop/Dark-chocolate-hazelnut-100.svg";
+import mobileCaillerDarkHazelnut100 from "../../public/cailler/Mobile/Dark-chocolate-hazelnut-100.jpg";
+
+import caillerDarkHazelnut200 from "../../public/cailler/Desktop/Dark-chocolate-hazelnut-200.svg";
+import mobileCaillerDarkHazelnut200 from "../../public/cailler/Mobile/Dark-chocolate-hazelnut-200.jpg";
+
+import caillerCremant100 from "../../public/cailler/Desktop/Cremant-100.svg";
+import mobileCaillerCremant100 from "../../public/cailler/Mobile/Cremant-100.jpg";
+
+import caillerCremant300 from "../../public/cailler/Desktop/Cremant-300.svg";
+import mobileCaillerCremant300 from "../../public/cailler/Mobile/Cremant-300.jpg";
+
+import caillerCrunchyCaramel100 from "../../public/cailler/Desktop/Crunchy-caramel-100.svg";
+import mobileCaillerCrunchyCaramel100 from "../../public/cailler/Mobile/Crunchy-caramel-100.jpg";
+
+import caillerDarkAlmond200 from "../../public/cailler/Desktop/Dark-chocolate-almonds-200.svg";
+import mobileCaillerDarkAlmond200 from "../../public/cailler/Mobile/Dark-chocolate-almonds-200.jpg";
+
+import caillerMilkAlmond100 from "../../public/cailler/Desktop/Milk-chocolate-almonds-100.svg";
+import mobileCaillerMilkAlmond100 from "../../public/cailler/Mobile/Milk-chocolate-almonds-100.jpg";
+
+import caillerMilkAlmond200 from "../../public/cailler/Desktop/Milk-chocolate-almonds-200.svg";
+import mobileCaillerMilkAlmond200 from "../../public/cailler/Mobile/Milk-chocolate-almonds-200.jpg";
+
+// import caillerDarkAlmond100 from "../../public/cailler/Desktop/Dark-chocolate-almonds-200.svg";
+// import mobileCaillerDarkAlmond100 from "../../public/cailler/Mobile/Dark-chocolate-almonds-200.jpg";
+
+import caillerMilkHazelnut200 from "../../public/cailler/Desktop/Milk-chocolate-hazelnut-200.svg";
+import mobileCaillerMilkHazelnut200 from "../../public/cailler/Mobile/Milk-chocolate-hazelnut-200.jpg";
+
+import caillerMilkHazelnut300 from "../../public/cailler/Desktop/Milk-chocolate-hazelnut-300.svg";
+import mobileCaillerMilkHazelnut300 from "../../public/cailler/Mobile/Milk-chocolate-hazelnut-300.jpg";
+
+import caillerMilkHazelnut100 from "../../public/cailler/Desktop/Milk-chocolate-hazelnut-100.svg";
+import mobileCaillerMilkHazelnut100 from "../../public/cailler/Mobile/Milk-chocolate-hazelnut-100.jpg";
+
+import caillerMilkHazelnut400 from "../../public/cailler/Desktop/Milk-chocolate-hazelnut-400.svg";
+import mobileCaillerMilkHazelnut400 from "../../public/cailler/Mobile/Milk-chocolate-hazelnut-400.jpg";
+
+const cailler = [
+  {
+    brand: "Cailler",
+    name: "Milk 100g",
+    product_description:
+      "Swiss Alp milk chocolate. Our chocolate recipe has been part of everyday life in Switzerland for decades. Combining fine cocoa beans with milk from local farms, it is a pure moment of simple delight.",
+    price_a: 30,
+    price_b: 20,
+    img: caillerMilk100,
+    mob_img: mobileCaillerMilk100,
+  },
+  {
+    brand: "Cailler",
+    name: "Milk 200g",
+    product_description:
+      "Swiss Alp milk chocolate. Our chocolate recipe has been part of everyday life in Switzerland for decades. Combining fine cocoa beans with milk from local farms, it is a pure moment of simple delight.",
+    price_a: 30,
+    price_b: 20,
+    img: caillerMilk200,
+    mob_img: mobileCaillerMilk200,
+  },
+  {
+    brand: "Cailler",
+    name: "Milk Almond 100g",
+    product_description:
+      "Delicate almonds bring moments of unexpected crunch to our smooth milk chocolate. Irresistibly enticing.",
+    price_a: 30,
+    price_b: 20,
+    img: caillerMilkAlmond100,
+    mob_img: mobileCaillerMilkAlmond100,
+  },
+  {
+    brand: "Cailler",
+    name: "Milk Almond 200g",
+    product_description:
+      "Delicate almonds bring moments of unexpected crunch to our smooth milk chocolate. Irresistibly enticing.",
+    price_a: 30,
+    price_b: 20,
+    img: caillerMilkAlmond200,
+    mob_img: mobileCaillerMilkAlmond200,
+  },
+  {
+    brand: "Cailler",
+    name: "Milk Hazelnut 100g",
+    product_description:
+      "Combining fine cocoa beans and milk from local farms with whole roasted hazelnuts, this is a heavenly mix of delicate crunch and smooth Cailler chocolate.",
+    price_a: 30,
+    price_b: 20,
+    img: caillerMilkHazelnut100,
+    mob_img: mobileCaillerMilkHazelnut100,
+  },
+  {
+    brand: "Cailler",
+    name: "Milk Hazelnut 200g",
+    product_description:
+      "Combining fine cocoa beans and milk from local farms with whole roasted hazelnuts, this is a heavenly mix of delicate crunch and smooth Cailler chocolate.",
+    price_a: 30,
+    price_b: 20,
+    img: caillerMilkHazelnut200,
+    mob_img: mobileCaillerMilkHazelnut200,
+  },
+  {
+    brand: "Cailler",
+    name: "Milk Hazelnut 300g",
+    product_description:
+      "Combining fine cocoa beans and milk from local farms with whole roasted hazelnuts, this is a heavenly mix of delicate crunch and smooth Cailler chocolate.",
+    price_a: 30,
+    price_b: 20,
+    img: caillerMilkHazelnut300,
+    mob_img: mobileCaillerMilkHazelnut300,
+  },
+  {
+    brand: "Cailler",
+    name: "Milk Hazelnut 400g",
+    product_description:
+      "Combining fine cocoa beans and milk from local farms with whole roasted hazelnuts, this is a heavenly mix of delicate crunch and smooth Cailler chocolate.",
+    price_a: 30,
+    price_b: 20,
+    img: caillerMilkHazelnut400,
+    mob_img: mobileCaillerMilkHazelnut400,
+  },
+  {
+    brand: "Cailler",
+    name: "Crunchy Caramel 100g",
+    product_description:
+      "The perfect combination of crunchy aerated cereals and salty caramel pieces, coated with delicious creamy Swiss Alpine milk chocolate. With its multi texture and irresistible taste, every bite is a burst of flavors that will satisfy your chocolate cravings.",
+    price_a: 30,
+    price_b: 20,
+    img: caillerCrunchyCaramel100,
+    mob_img: mobileCaillerCrunchyCaramel100,
+  },
+  {
+    brand: "Cailler",
+    name: "Dark Hazelnut 100g",
+    product_description:
+      "Crisp toasted hazelnuts are immersed in sweet, smooth dark chocolate. A crunchy combination of harmonious ingredients.",
+    price_a: 30,
+    price_b: 20,
+    img: caillerDarkHazelnut100,
+    mob_img: mobileCaillerDarkHazelnut100,
+  },
+  {
+    brand: "Cailler",
+    name: "Dark Hazelnut 200g",
+    product_description:
+      "Crisp toasted hazelnuts are immersed in sweet, smooth dark chocolate. A crunchy combination of harmonious ingredients.",
+    price_a: 30,
+    price_b: 20,
+    img: caillerDarkHazelnut200,
+    mob_img: mobileCaillerDarkHazelnut200,
+  },
+  {
+    brand: "Cailler",
+    name: "Dark Almond 200g",
+    product_description:
+      "Delicate almonds bring moments of unexpected crunch to our smooth dark chocolate. Irresistibly enticing.",
+    price_a: 30,
+    price_b: 20,
+    img: caillerDarkAlmond200,
+    mob_img: mobileCaillerDarkAlmond200,
+  },
+  {
+    brand: "Cailler",
+    name: "Cremant 100g",
+    product_description:
+      "As dark as night, our dark chocolate melts sweetly on the tongue. A careful composition of selected fine cocoa beans, it’s the dark chocolate of your dreams..",
+    price_a: 30,
+    price_b: 20,
+    img: caillerCremant100,
+    mob_img: mobileCaillerCremant100,
+  },
+  {
+    brand: "Cailler",
+    name: "Cremant 300g",
+    product_description:
+      "As dark as night, our dark chocolate melts sweetly on the tongue. A careful composition of selected fine cocoa beans, it’s the dark chocolate of your dreams.",
+    price_a: 30,
+    price_b: 20,
+    img: caillerCremant300,
+    mob_img: mobileCaillerCremant300,
+  },
+];
+
+export default cailler;
