b9f015d21c0f28c9ba6108719c701b27f0d0f968
asfundb <muhammedbagha5@gmail.com>
2023-12-21 14:24:44 +0400

    cailler data placement

---
+++ src/data/en/cailler.js
@@ -0,0 +1,213 @@
+import caillerMilk100 from "../../../public/cailler/Desktop/Milk-chocolate-100.svg";
+import mobileCaillerMilk100 from "../../../public/cailler/Mobile/Milk-chocolate-100.jpg";
+
+import caillerMilk200 from "../../../public/cailler/Desktop/Milk-chocolate-200.svg";
+import mobileCaillerMilk200 from "../../../public/cailler/Mobile/Milk-chocolate-200.jpg";
+
+import caillerDarkHazelnut100 from "../../../public/cailler/Desktop/Dark-chocolate-hazelnut-100.svg";
+import mobileCaillerDarkHazelnut100 from "../../../public/cailler/Mobile/Dark-chocolate-hazelnut-100.jpg";
+
+import caillerDarkHazelnut200 from "../../../public/cailler/Desktop/Dark-chocolate-hazelnut-200.svg";
+import mobileCaillerDarkHazelnut200 from "../../../public/cailler/Mobile/Dark-chocolate-hazelnut-200.jpg";
+
+import caillerCremant100 from "../../../public/cailler/Desktop/Cremant-100.svg";
+import mobileCaillerCremant100 from "../../../public/cailler/Mobile/Cremant-100.jpg";
+
+import caillerCremant300 from "../../../public/cailler/Desktop/Cremant-300.svg";
+import mobileCaillerCremant300 from "../../../public/cailler/Mobile/Cremant-300.jpg";
+
+import caillerCrunchyCaramel100 from "../../../public/cailler/Desktop/Crunchy-caramel-100.svg";
+import mobileCaillerCrunchyCaramel100 from "../../../public/cailler/Mobile/Crunchy-caramel-100.jpg";
+
+import caillerDarkAlmond200 from "../../../public/cailler/Desktop/Dark-chocolate-almonds-200.svg";
+import mobileCaillerDarkAlmond200 from "../../../public/cailler/Mobile/Dark-chocolate-almonds-200.jpg";
+
+import caillerMilkAlmond100 from "../../../public/cailler/Desktop/Milk-chocolate-almonds-100.svg";
+import mobileCaillerMilkAlmond100 from "../../../public/cailler/Mobile/Milk-chocolate-almonds-100.jpg";
+
+import caillerMilkAlmond200 from "../../../public/cailler/Desktop/Milk-chocolate-almonds-200.svg";
+import mobileCaillerMilkAlmond200 from "../../../public/cailler/Mobile/Milk-chocolate-almonds-200.jpg";
+
+import caillerMilkHazelnut200 from "../../../public/cailler/Desktop/Milk-chocolate-hazelnut-200.svg";
+import mobileCaillerMilkHazelnut200 from "../../../public/cailler/Mobile/Milk-chocolate-hazelnut-200.jpg";
+
+import caillerMilkHazelnut300 from "../../../public/cailler/Desktop/Milk-chocolate-hazelnut-300.svg";
+import mobileCaillerMilkHazelnut300 from "../../../public/cailler/Mobile/Milk-chocolate-hazelnut-300.jpg";
+
+import caillerMilkHazelnut100 from "../../../public/cailler/Desktop/Milk-chocolate-hazelnut-100.svg";
+import mobileCaillerMilkHazelnut100 from "../../../public/cailler/Mobile/Milk-chocolate-hazelnut-100.jpg";
+
+import caillerChocmel100 from "../../../public/cailler/Desktop/Milk-chocolate-chocmel-100-new.svg";
+import mobileCaillerChocmel100 from "../../../public/cailler/Mobile/Milk-chocolate-chocmel-100-new.jpg";
+
+import caillerFrigor100 from "../../../public/cailler/Desktop/Milk-chocolate-frigor-100-new.svg";
+import mobileCaillerFrigor100 from "../../../public/cailler/Mobile/Milk-chocolate-frigor-100-new.jpg";
+
+import caillerRayon100 from "../../../public/cailler/Desktop/Milk-chocolate-rayon-100-new.svg";
+import mobileCaillerRayon100 from "../../../public/cailler/Mobile/Milk-chocolate-rayon-100-new.jpg";
+//
+
+const cailler = [
+  {
+    brand: "Cailler",
+    name: "Milk 100g",
+    product_description:
+      "Swiss Alp milk chocolate. Our chocolate recipe has been part of everyday life in Switzerland for decades. Combining fine cocoa beans with milk from local farms, it is a pure moment of simple delight.",
+    price_a: 11,
+    price_b: 16,
+    img: caillerMilk100,
+    mob_img: mobileCaillerMilk100,
+  },
+  {
+    brand: "Cailler",
+    name: "Milk 200g",
+    product_description:
+      "Swiss Alp milk chocolate. Our chocolate recipe has been part of everyday life in Switzerland for decades. Combining fine cocoa beans with milk from local farms, it is a pure moment of simple delight.",
+    price_a: 15,
+    price_b: 22,
+    img: caillerMilk100,
+    mob_img: mobileCaillerMilk100,
+  },
+  {
+    brand: "Cailler",
+    name: "Milk Almond 100g",
+    product_description:
+      "Delicate almonds bring moments of unexpected crunch to our smooth milk chocolate. Irresistibly enticing.",
+    price_a: 11,
+    price_b: 16,
+    img: caillerMilkAlmond100,
+    mob_img: mobileCaillerMilkAlmond100,
+  },
+  {
+    brand: "Cailler",
+    name: "Milk Almond 200g",
+    product_description:
+      "Delicate almonds bring moments of unexpected crunch to our smooth milk chocolate. Irresistibly enticing.",
+    price_a: 15,
+    price_b: 22,
+    img: caillerMilkAlmond200,
+    mob_img: mobileCaillerMilkAlmond200,
+  },
+  {
+    brand: "Cailler",
+    name: "Milk Hazelnut 100g",
+    product_description:
+      "Combining fine cocoa beans and milk from local farms with whole roasted hazelnuts, this is a heavenly mix of delicate crunch and smooth Cailler chocolate.",
+    price_a: 11,
+    price_b: 16,
+    img: caillerMilkHazelnut100,
+    mob_img: mobileCaillerMilkHazelnut100,
+  },
+  {
+    brand: "Cailler",
+    name: "Milk Hazelnut 200g",
+    product_description:
+      "Combining fine cocoa beans and milk from local farms with whole roasted hazelnuts, this is a heavenly mix of delicate crunch and smooth Cailler chocolate.",
+    price_a: 15,
+    price_b: 22,
+    img: caillerMilkHazelnut200,
+    mob_img: mobileCaillerMilkHazelnut200,
+  },
+  {
+    brand: "Cailler",
+    name: "Milk Hazelnut 300g",
+    product_description:
+      "Combining fine cocoa beans and milk from local farms with whole roasted hazelnuts, this is a heavenly mix of delicate crunch and smooth Cailler chocolate.",
+    price_a: 28,
+    price_b: 38,
+    img: caillerMilkHazelnut300,
+    mob_img: mobileCaillerMilkHazelnut300,
+  },
+  {
+    brand: "Cailler",
+    name: "Crunchy Caramel 100g",
+    product_description:
+      "The perfect combination of crunchy aerated cereals and salty caramel pieces, coated with delicious creamy Swiss Alpine milk chocolate. With its multi texture and irresistible taste, every bite is a burst of flavors that will satisfy your chocolate cravings.",
+    price_a: 11,
+    price_b: 16,
+    img: caillerCrunchyCaramel100,
+    mob_img: mobileCaillerCrunchyCaramel100,
+  },
+  {
+    brand: "Cailler",
+    name: "Chocmel 100g",
+    product_description:
+      "Its multi-faceted charm gives you a melt-in-your-mouth experience. A sea of ​​deliciously crunchy slivers surprises with wonderful honey and almond nuances.",
+    price_a: 11,
+    price_b: 16,
+    img: caillerChocmel100,
+    mob_img: mobileCaillerChocmel100,
+  },
+  {
+    brand: "Cailler",
+    name: "Rayon 100g",
+    product_description:
+      "From the bursting of delicate air bubbles to the splintering of honey nougat to the melting of milk chocolate. The Rayon is pleasure in crescendo.",
+    price_a: 11,
+    price_b: 16,
+    img: caillerRayon100,
+    mob_img: mobileCaillerRayon100,
+  },
+  {
+    brand: "Cailler",
+    name: "Frigor 100g",
+    product_description:
+      "There is nothing like Frigor. It has such a delicate consistency. Milk chocolate melts on the outside and a cream filling made from hazelnuts and almonds melts on the inside",
+    price_a: 11,
+    price_b: 16,
+    img: caillerFrigor100,
+    mob_img: mobileCaillerFrigor100,
+  },
+  {
+    brand: "Cailler",
+    name: "Dark Hazelnut 100g",
+    product_description:
+      "Crisp toasted hazelnuts are immersed in sweet, smooth dark chocolate. A crunchy combination of harmonious ingredients.",
+    price_a: 11,
+    price_b: 16,
+    img: caillerDarkHazelnut100,
+    mob_img: mobileCaillerDarkHazelnut100,
+  },
+  {
+    brand: "Cailler",
+    name: "Dark Hazelnut 200g",
+    product_description:
+      "Crisp toasted hazelnuts are immersed in sweet, smooth dark chocolate. A crunchy combination of harmonious ingredients.",
+    price_a: 15,
+    price_b: 22,
+    img: caillerDarkHazelnut200,
+    mob_img: mobileCaillerDarkHazelnut200,
+  },
+  {
+    brand: "Cailler",
+    name: "Dark Almond 200g",
+    product_description:
+      "Delicate almonds bring moments of unexpected crunch to our smooth dark chocolate. Irresistibly enticing.",
+    price_a: 15,
+    price_b: 22,
+    img: caillerDarkAlmond200,
+    mob_img: mobileCaillerDarkAlmond200,
+  },
+  {
+    brand: "Cailler",
+    name: "Cremant 100g",
+    product_description:
+      "As dark as night, our dark chocolate melts sweetly on the tongue. A careful composition of selected fine cocoa beans, it’s the dark chocolate of your dreams..",
+    price_a: 11,
+    price_b: 16,
+    img: caillerCremant100,
+    mob_img: mobileCaillerCremant100,
+  },
+  {
+    brand: "Cailler",
+    name: "Cremant 300g",
+    product_description:
+      "As dark as night, our dark chocolate melts sweetly on the tongue. A careful composition of selected fine cocoa beans, it’s the dark chocolate of your dreams.",
+    price_a: 28,
+    price_b: 38,
+    img: caillerCremant300,
+    mob_img: mobileCaillerCremant300,
+  },
+];
+
+export default cailler;
