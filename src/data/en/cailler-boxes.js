93556c4c44d0726f1b2f57e560f063bff2055438
asfundb <muhammedbagha5@gmail.com>
2023-12-22 21:07:47 +0400

    add 127g variation

--- src/data/en/cailler-boxes.js
+++ src/data/en/cailler-boxes.js
@@ -1,5 +1,7 @@
 import femina122 from "../../../public/cailler-boxes/Desktop/femina-122.svg";
 import mobileFemina122 from "../../../public/cailler-boxes/Mobile/femina-122.png";
+import femina127 from "../../../public/cailler-boxes/Desktop/femina-127.svg";
+import mobileFemina127 from "../../../public/cailler-boxes/Mobile/femina-127.png";
 import femina250 from "../../../public/cailler-boxes/Desktop/femina-250.svg";
 import mobileFemina250 from "../../../public/cailler-boxes/Mobile/femina-250.png";
 import femina500 from "../../../public/cailler-boxes/Desktop/femina-500.svg";
@@ -7,6 +9,8 @@ import mobileFemina500 from "../../../public/cailler-boxes/Mobile/femina-500.png
 
 import ambassador122 from "../../../public/cailler-boxes/Desktop/ambassador-122.svg";
 import mobileAmbassador122 from "../../../public/cailler-boxes/Mobile/ambassador-122.png";
+import ambassador127 from "../../../public/cailler-boxes/Desktop/ambassador-127.svg";
+import mobileAmbassador127 from "../../../public/cailler-boxes/Mobile/ambassador-127.png";
 import ambassador250 from "../../../public/cailler-boxes/Desktop/ambassador-250.svg";
 import mobileAmbassador250 from "../../../public/cailler-boxes/Mobile/ambassador-250.png";
 import ambassador490 from "../../../public/cailler-boxes/Desktop/ambassador-490.svg";
@@ -22,6 +26,16 @@ const cailler = [
     price_b: 50,
     img: femina122,
     mob_img: mobileFemina122,
+  },
+  {
+    brand: "Cailler",
+    name: "Femina 127g",
+    product_description:
+      "This is what pleasure looks like. Unwrap the famous Femina foil to reveal the seductive mixture of smooth milk chocolate and melting hazelnut cream.",
+    price_a: 20,
+    price_b: 50,
+    img: femina127,
+    mob_img: mobileFemina127,
   },
   {
     brand: "Cailler",
@@ -52,6 +66,16 @@ const cailler = [
     price_b: 50,
     img: ambassador122,
     mob_img: mobileAmbassador122,
+  },
+  {
+    brand: "Cailler",
+    name: "Ambassador 127g",
+    product_description:
+      "An elegant assortment of classic pralines, this is a smooth and satisfying collection of Cailler favourites. As a gift or a treat, they are the perfect introduction to the world of Swiss chocolate.",
+    price_a: 20,
+    price_b: 50,
+    img: ambassador127,
+    mob_img: mobileAmbassador127,
   },
   {
     brand: "Cailler",
