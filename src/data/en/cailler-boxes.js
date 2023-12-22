b85e25a3eedee9c9337b1b6790e9cbda5b23b506
asfundb <muhammedbagha5@gmail.com>
2023-12-22 20:54:32 +0400

    chocolate boxes

---
+++ src/data/en/cailler-boxes.js
@@ -0,0 +1,78 @@
+import femina122 from "../../../public/cailler-boxes/Desktop/femina-122.svg";
+import mobileFemina122 from "../../../public/cailler-boxes/Mobile/femina-122.png";
+import femina250 from "../../../public/cailler-boxes/Desktop/femina-250.svg";
+import mobileFemina250 from "../../../public/cailler-boxes/Mobile/femina-250.png";
+import femina500 from "../../../public/cailler-boxes/Desktop/femina-500.svg";
+import mobileFemina500 from "../../../public/cailler-boxes/Mobile/femina-500.png";
+
+import ambassador122 from "../../../public/cailler-boxes/Desktop/ambassador-122.svg";
+import mobileAmbassador122 from "../../../public/cailler-boxes/Mobile/ambassador-122.png";
+import ambassador250 from "../../../public/cailler-boxes/Desktop/ambassador-250.svg";
+import mobileAmbassador250 from "../../../public/cailler-boxes/Mobile/ambassador-250.png";
+import ambassador490 from "../../../public/cailler-boxes/Desktop/ambassador-490.svg";
+import mobileAmbassador490 from "../../../public/cailler-boxes/Mobile/ambassador-490.png";
+
+const cailler = [
+  {
+    brand: "Cailler",
+    name: "Femina 122g",
+    product_description:
+      "This is what pleasure looks like. Unwrap the famous Femina foil to reveal the seductive mixture of smooth milk chocolate and melting hazelnut cream.",
+    price_a: 20,
+    price_b: 50,
+    img: femina122,
+    mob_img: mobileFemina122,
+  },
+  {
+    brand: "Cailler",
+    name: "Femina 250g",
+    product_description:
+      "This is what pleasure looks like. Unwrap the famous Femina foil to reveal the seductive mixture of smooth milk chocolate and melting hazelnut cream.",
+    price_a: 40,
+    price_b: 80,
+    img: femina250,
+    mob_img: mobileFemina250,
+  },
+  {
+    brand: "Cailler",
+    name: "Femina 500g",
+    product_description:
+      "This is what pleasure looks like. Unwrap the famous Femina foil to reveal the seductive mixture of smooth milk chocolate and melting hazelnut cream.",
+    price_a: 70,
+    price_b: 125,
+    img: femina500,
+    mob_img: mobileFemina500,
+  },
+  {
+    brand: "Cailler",
+    name: "Ambassador 122g",
+    product_description:
+      "An elegant assortment of classic pralines, this is a smooth and satisfying collection of Cailler favourites. As a gift or a treat, they are the perfect introduction to the world of Swiss chocolate.",
+    price_a: 20,
+    price_b: 50,
+    img: ambassador122,
+    mob_img: mobileAmbassador122,
+  },
+  {
+    brand: "Cailler",
+    name: "Ambassador 250g",
+    product_description:
+      "An elegant assortment of classic pralines, this is a smooth and satisfying collection of Cailler favourites. As a gift or a treat, they are the perfect introduction to the world of Swiss chocolate.",
+    price_a: 40,
+    price_b: 80,
+    img: ambassador250,
+    mob_img: mobileAmbassador250,
+  },
+  {
+    brand: "Cailler",
+    name: "Ambassador 490g",
+    product_description:
+      "An elegant assortment of classic pralines, this is a smooth and satisfying collection of Cailler favourites. As a gift or a treat, they are the perfect introduction to the world of Swiss chocolate.",
+    price_a: 70,
+    price_b: 125,
+    img: ambassador490,
+    mob_img: mobileAmbassador490,
+  },
+];
+
+export default cailler;
---
+++ src/data/en/recettes-boxes.js
@@ -0,0 +1,77 @@
+import bigMilkAssorted from "../../../public/recettes-boxes/Desktop/big-milk-assorted.svg";
+import mobileBigMilkAssorted from "../../../public/recettes-boxes/Mobile/big-milk-assorted.png";
+import bigAssorted from "../../../public/recettes-boxes/Desktop/big-assorted.svg";
+import mobileBigAssorted from "../../../public/recettes-boxes/Mobile/big-assorted.png";
+import smallAssorted from "../../../public/recettes-boxes/Desktop/small-assorted.svg";
+import mobileSmallAssorted from "../../../public/recettes-boxes/Mobile/small-assorted.png";
+import smallCocao from "../../../public/recettes-boxes/Desktop/small-cocao-beans.svg";
+import mobileSmallCocao from "../../../public/recettes-boxes/Mobile/small-cocao-beans.png";
+import smallHazelnut from "../../../public/recettes-boxes/Desktop/small-hazelnut.svg";
+import mobileSmallHazelnut from "../../../public/recettes-boxes/Mobile/small-hazelnut.png";
+import smallSaltedCaramel from "../../../public/recettes-boxes/Desktop/small-salted-caramel.svg";
+import mobileSmallSaltedCaramel from "../../../public/recettes-boxes/Mobile/small-salted-caramel.png";
+
+const recettesBoxes = [
+  {
+    brand: "Recettes De L'Atelier",
+    name: "Milk Chocolate Bites 400g",
+    product_description:
+      "Unique chocolate expertise can be found in this assortment of three gourmet bites coated in sweet milk chocolate: Hazelnut pieces, cocoa beans and salted caramel.",
+    price_a: 45,
+    price_b: 60,
+    img: bigMilkAssorted,
+    mob_img: mobileBigMilkAssorted,
+  },
+  {
+    brand: "Recettes De L'Atelier",
+    name: "Chocolate Bites 400g",
+    product_description:
+      "A unique chocolate expertise to be found in this assortment of four gourmet bites: Hazelnut pieces, salted caramel, cocoa beans and intense raspberry.",
+    price_a: 45,
+    price_b: 60,
+    img: bigAssorted,
+    mob_img: mobileBigAssorted,
+  },
+  {
+    brand: "Recettes De L'Atelier",
+    name: "Assorted Bites 265g",
+    product_description:
+      "Discover our delicious Les Recettes de lAtelier bites in this format bringing together our Hazelnut / Caramel / Cocoa bean recipes. A soft center, a crunchy shell and pieces of dried fruit/cocoa beans on top. Made from pure cocoa butter chocolate",
+    price_a: 32,
+    price_b: 42,
+    img: smallAssorted,
+    mob_img: mobileSmallAssorted,
+  },
+  {
+    brand: "Recettes De L'Atelier",
+    name: "Cocao Bean 265g",
+    product_description:
+      "Discover our delicious bites filled with cocoa ganache, with a dark chocolate shell sprinkled with cocoa bean chips. Made from pure cocoa butter chocolate. ",
+    price_a: 32,
+    price_b: 42,
+    img: smallCocao,
+    mob_img: mobileSmallCocao,
+  },
+  {
+    brand: "Recettes De L'Atelier",
+    name: "Hazelnut Bites 265g",
+    product_description:
+      "Discover our delicious bites with a creamy praline center, milk chocolate shell sprinkled with crunchy hazelnut pieces. Made from pure cocoa butter chocolate.",
+    price_a: 32,
+    price_b: 42,
+    img: smallHazelnut,
+    mob_img: mobileSmallHazelnut,
+  },
+  {
+    brand: "Recettes De L'Atelier",
+    name: "Slated Caramel Bites 265g",
+    product_description:
+      "Discover our delicious bites with a melting center and crunchy inclusions. Made from pure cocoa butter chocolate",
+    price_a: 32,
+    price_b: 42,
+    img: smallSaltedCaramel,
+    mob_img: mobileSmallSaltedCaramel,
+  },
+];
+
+export default recettesBoxes;
