60824c0e6fa6827ba123483d96eace61e1757fbf
asfundb <muhammedbagha5@gmail.com>
2024-01-02 15:13:14 +0400

    cailler bars arabic

---
+++ src/data/ar/cailler.js
@@ -0,0 +1,213 @@
+import caillerMilk100 from "../../../public/cailler/Desktop/Milk-chocolate-100.svg";
+import mobileCaillerMilk100 from "../../../public/cailler/Mobile/Milk-chocolate-100.jpg";
+
+import caillerMilk200 from "../../../public/cailler/Desktop/Milk-chocolate-200.svg";
+import mobileCaillerMilk200 from "../../../public/cailler/Mobile/Milk-chocolate-200.jpg";
+
+import caillerDarkHazelnut100 from "../../../public/cailler/Desktop/Dark-chocolate-hazelnut-100.svg";
+import mobileCaillerDarkHazelnut100 from "../../../public/cailler/Mobile/Dark-chocolate-hazelnut-100.jpg";
+
+import caillerDarkHazelnut200 from "../../../public/cailler/Desktop/Dark-chocolate-hazelnut-200.svg";
+import mobileCaillerDarkHazelnut200 from "../../../public/cailler/Mobile/Dark-chocolate-hazelnut-200.jpg";
+
+import caillerCremant100 from "../../../public/cailler/Desktop/Cremant-100.svg";
+import mobileCaillerCremant100 from "../../../public/cailler/Mobile/Cremant-100.jpg";
+
+import caillerCremant300 from "../../../public/cailler/Desktop/Cremant-300.svg";
+import mobileCaillerCremant300 from "../../../public/cailler/Mobile/Cremant-300.jpg";
+
+import caillerCrunchyCaramel100 from "../../../public/cailler/Desktop/Crunchy-caramel-100.svg";
+import mobileCaillerCrunchyCaramel100 from "../../../public/cailler/Mobile/Crunchy-caramel-100.jpg";
+
+import caillerDarkAlmond200 from "../../../public/cailler/Desktop/Dark-chocolate-almonds-200.svg";
+import mobileCaillerDarkAlmond200 from "../../../public/cailler/Mobile/Dark-chocolate-almonds-200.jpg";
+
+import caillerMilkAlmond100 from "../../../public/cailler/Desktop/Milk-chocolate-almonds-100.svg";
+import mobileCaillerMilkAlmond100 from "../../../public/cailler/Mobile/Milk-chocolate-almonds-100.jpg";
+
+import caillerMilkAlmond200 from "../../../public/cailler/Desktop/Milk-chocolate-almonds-200.svg";
+import mobileCaillerMilkAlmond200 from "../../../public/cailler/Mobile/Milk-chocolate-almonds-200.jpg";
+
+import caillerMilkHazelnut200 from "../../../public/cailler/Desktop/Milk-chocolate-hazelnut-200.svg";
+import mobileCaillerMilkHazelnut200 from "../../../public/cailler/Mobile/Milk-chocolate-hazelnut-200.jpg";
+
+import caillerMilkHazelnut300 from "../../../public/cailler/Desktop/Milk-chocolate-hazelnut-300.svg";
+import mobileCaillerMilkHazelnut300 from "../../../public/cailler/Mobile/Milk-chocolate-hazelnut-300.jpg";
+
+import caillerMilkHazelnut100 from "../../../public/cailler/Desktop/Milk-chocolate-hazelnut-100.svg";
+import mobileCaillerMilkHazelnut100 from "../../../public/cailler/Mobile/Milk-chocolate-hazelnut-100.jpg";
+
+import caillerChocmel100 from "../../../public/cailler/Desktop/Milk-chocolate-chocmel-100-new.svg";
+import mobileCaillerChocmel100 from "../../../public/cailler/Mobile/Milk-chocolate-chocmel-100-new.jpg";
+
+import caillerFrigor100 from "../../../public/cailler/Desktop/Milk-chocolate-frigor-100-new.svg";
+import mobileCaillerFrigor100 from "../../../public/cailler/Mobile/Milk-chocolate-frigor-100-new.jpg";
+
+import caillerRayon100 from "../../../public/cailler/Desktop/Milk-chocolate-rayon-100-new.svg";
+import mobileCaillerRayon100 from "../../../public/cailler/Mobile/Milk-chocolate-rayon-100-new.jpg";
+//
+
+const cailler = [
+  {
+    brand: "Cailler",
+    name: "حليب 100 جرام",
+    product_description:
+      "شوكولاتة جبال الألب السويسرية. وصفة الشوكولاتة خاصتنا جزءٌ من الحياة اليومية في سويسرا منذ عقودٍ. بوصفها مزيجًا من حبوب الكاكاو المطحونة والحليب من المزارع المحلية، هي لحظةٌ خالصةٌ من المتعة البسيطة.",
+    price_a: "١١",
+    price_b: "١٦",
+    img: caillerMilk100,
+    mob_img: mobileCaillerMilk100,
+  },
+  {
+    brand: "Cailler",
+    name: "حليب 200 جرام",
+    product_description:
+      "شوكولاتة جبال الألب السويسرية. وصفة الشوكولاتة خاصتنا جزءٌ من الحياة اليومية في سويسرا منذ عقودٍ. بوصفها مزيجًا من حبوب الكاكاو المطحونة والحليب من المزارع المحلية، هي لحظةٌ خالصةٌ من المتعة البسيطة",
+    price_a: "١٥",
+    price_b: "٢٢",
+    img: caillerMilk100,
+    mob_img: mobileCaillerMilk100,
+  },
+  {
+    brand: "Cailler",
+    name: "حليب ولوز 100 جرام",
+    product_description:
+      "اللوز الهش يأتي بلحظاتٍ من القرمشة غير المتوقعة إلى الشوكولاتة الناعمة بالحليب خاصتنا. مغريةٌ بشكلٍ لا يقاوم",
+    price_a: "١١",
+    price_b: "١٦",
+    img: caillerMilkAlmond100,
+    mob_img: mobileCaillerMilkAlmond100,
+  },
+  {
+    brand: "Cailler",
+    name: "حليب ولوز 200 جرام",
+    product_description:
+      "اللوز الهش يأتي بلحظاتٍ من القرمشة غير المتوقعة إلى الشوكولاتة الناعمة بالحليب خاصتنا. مغريةٌ بشكلٍ لا يقاوم",
+    price_a: "١٥",
+    price_b: "٢٢",
+    img: caillerMilkAlmond200,
+    mob_img: mobileCaillerMilkAlmond200,
+  },
+  {
+    brand: "Cailler",
+    name: "حليب وبندق 100 جرام",
+    product_description:
+      "كمزيجٍ من حبوب الكاكاو المطحونة والحليب من المزارع المحلية بحبات البندق المحمصة الكاملة، هذا الخليط من القرمشة الخفيفة وشوكولاتة Cailler الناعمة سينقلك إلى الجنة",
+    price_a: "١١",
+    price_b: "١٦",
+    img: caillerMilkHazelnut100,
+    mob_img: mobileCaillerMilkHazelnut100,
+  },
+  {
+    brand: "Cailler",
+    name: "حليب وبندق 200 جرام",
+    product_description:
+      "كمزيجٍ من حبوب الكاكاو المطحونة والحليب من المزارع المحلية بحبات البندق المحمصة الكاملة، هذا الخليط من القرمشة الخفيفة وشوكولاتة Cailler الناعمة سينقلك إلى الجنة",
+    price_a: "١٥",
+    price_b: "٢٢",
+    img: caillerMilkHazelnut200,
+    mob_img: mobileCaillerMilkHazelnut200,
+  },
+  {
+    brand: "Cailler",
+    name: "حليب وبندق 300 جرام،",
+    product_description:
+      "كمزيجٍ من حبوب الكاكاو المطحونة والحليب من المزارع المحلية بحبات البندق المحمصة الكاملة، هذا الخليط من القرمشة الخفيفة وشوكولاتة Cailler الناعمة سينقلك إلى الجنة",
+    price_a: "٢٨",
+    price_b: "٣٨",
+    img: caillerMilkHazelnut300,
+    mob_img: mobileCaillerMilkHazelnut300,
+  },
+  {
+    brand: "Cailler",
+    name: "كاراميل مقرمش 100 جرام",
+    product_description:
+      "الخليط المثالي من رقائق الذرة المهواة المشرمشة وقطع الكاراميل المالحة، مغطاةٌ بشوكولاتة جبال الألب السويسرية الكريمية اللذيذة بالحليب. بطبقاتها المتعددة ومذاقها الذي لا يقاوم، كل قضمةٍ هي دفقةٌ من النكهات التي ستشبع رغبات الشوكولاتة الملحة لديك.",
+    price_a: "١١",
+    price_b: "١٦",
+    img: caillerCrunchyCaramel100,
+    mob_img: mobileCaillerCrunchyCaramel100,
+  },
+  {
+    brand: "Cailler",
+    name: "مزيج الشوكولاتة والحليب 100 جرام",
+    product_description:
+      "يجعلها سحرها بأوجهه المتعددة تذوب في فمك. بحرٌ من المفاجآت المجزأة المقرمشة بشكلٍ لذيذٍ مع لمسات العسل واللوز الرائعة.",
+    price_a: "١١",
+    price_b: "١٦",
+    img: caillerChocmel100,
+    mob_img: mobileCaillerChocmel100,
+  },
+  {
+    brand: "Cailler",
+    name: "رايون 100 جرام،",
+    product_description:
+      "بدءًا بانفجار فقاقيع الهواء الرقيقة ومرورًا بتفتت النوجة بالكاراميل وانتهاءًا بذوبان الشوكولاتة بالحليب. الرايون لذةٌ متزايدةٌ",
+    price_a: "١١",
+    price_b: "١٦",
+    img: caillerRayon100,
+    mob_img: mobileCaillerRayon100,
+  },
+  {
+    brand: "Cailler",
+    name: "فريجور 100 جرام",
+    product_description:
+      "لا يوجد مثيلٌ لفريجور. لها قوامٌ رقيقٌ للغاية. تذوب الشوكولاتة بالحليب من الخارج وتذوب حشوةٌ كريميةٌ من البندق واللوز في الداخل",
+    price_a: "١١",
+    price_b: "١٦",
+    img: caillerFrigor100,
+    mob_img: mobileCaillerFrigor100,
+  },
+  {
+    brand: "Cailler",
+    name: "داكنة بالبندق 100 جرام",
+    product_description:
+      "يغطَى البندق المحمص الهش بالشوكولاتة الداكنة الناعمة، اللذيذة. خليطٌ مقرمشٌ من المكونات المتجانسة",
+    price_a: "١١",
+    price_b: "١٦",
+    img: caillerDarkHazelnut100,
+    mob_img: mobileCaillerDarkHazelnut100,
+  },
+  {
+    brand: "Cailler",
+    name: "داكنة بالبندق 200 جرام،",
+    product_description:
+      "يغَطى البندق المحمص الهش بالشوكولاتة الداكنة الناعمة، اللذيذة. خليطٌ مقرمشٌ من المكونات المتجانسة",
+    price_a: "١٥",
+    price_b: "٢٢",
+    img: caillerDarkHazelnut200,
+    mob_img: mobileCaillerDarkHazelnut200,
+  },
+  {
+    brand: "Cailler",
+    name: "داكنة باللوز 200 جرام،",
+    product_description:
+      "اللوز الهش يأتي بلحظاتٍ من القرمشة غير المتوقعة إلى الشوكولاتة الداكنة الناعمة خاصتنا. مغريةٌ بشكلٍ لا يقاوم",
+    price_a: "١٥",
+    price_b: "٢٢",
+    img: caillerDarkAlmond200,
+    mob_img: mobileCaillerDarkAlmond200,
+  },
+  {
+    brand: "Cailler",
+    name: "كريمانت 100 جرام،",
+    product_description:
+      "داكنةٌ كجوف الليل، الشوكولاتة الداكنة خاصتنا تذوب بعذوبةٍ على اللسان. بوصفها تركيبةً مدروسةً من حبوب الكاكاو المطحونة المنتقاة، هي الشوكولاتة الداكنة التي تحلم بها",
+    price_a: "١١",
+    price_b: "١٦",
+    img: caillerCremant100,
+    mob_img: mobileCaillerCremant100,
+  },
+  {
+    brand: "Cailler",
+    name: "كريمانت 300 جرام،",
+    product_description:
+      "داكنةٌ كجوف الليل، الشوكولاتة الداكنة خاصتنا تذوب بعذوبةٍ على اللسان. بوصفها تركيبةً مدروسةً من حبوب الكاكاو المطحونة المنتقاة، هي الشوكولاتة الداكنة التي تحلم بها",
+    price_a: "٢٨",
+    price_b: "٣٨",
+    img: caillerCremant300,
+    mob_img: mobileCaillerCremant300,
+  },
+];
+
+export default cailler;
