be6d777e757530e5b31a4aec80e2e8ef8d847943
asfundb <muhammedbagha5@gmail.com>
2024-01-04 13:21:14 +0400

    ksa cailler boxes

---
+++ src/data/ar/cailler-boxes.js
@@ -0,0 +1,103 @@
+import femina122 from "../../../public/cailler-boxes/Desktop/femina-122.svg";
+import mobileFemina122 from "../../../public/cailler-boxes/Mobile/femina-122.png";
+import femina127 from "../../../public/cailler-boxes/Desktop/femina-127.svg";
+import mobileFemina127 from "../../../public/cailler-boxes/Mobile/femina-127.png";
+import femina250 from "../../../public/cailler-boxes/Desktop/femina-250.svg";
+import mobileFemina250 from "../../../public/cailler-boxes/Mobile/femina-250.png";
+import femina500 from "../../../public/cailler-boxes/Desktop/femina-500.svg";
+import mobileFemina500 from "../../../public/cailler-boxes/Mobile/femina-500.png";
+
+import ambassador122 from "../../../public/cailler-boxes/Desktop/ambassador-122.svg";
+import mobileAmbassador122 from "../../../public/cailler-boxes/Mobile/ambassador-122.png";
+import ambassador127 from "../../../public/cailler-boxes/Desktop/ambassador-127.svg";
+import mobileAmbassador127 from "../../../public/cailler-boxes/Mobile/ambassador-127.png";
+import ambassador250 from "../../../public/cailler-boxes/Desktop/ambassador-250.svg";
+import mobileAmbassador250 from "../../../public/cailler-boxes/Mobile/ambassador-250.png";
+import ambassador490 from "../../../public/cailler-boxes/Desktop/ambassador-490.svg";
+import mobileAmbassador490 from "../../../public/cailler-boxes/Mobile/ambassador-490.png";
+
+const cailler = [
+  {
+    brand: "Cailler",
+    name: "Femina 122 جرام",
+    product_description:
+      "حتى تكشف عن الخليط المغري من شوكولاتة الحليب الناعمة وكريمة البندق الذائبة Femina هكذا تبدو اللذة. أزل غلاف الفويل الشهير ل",
+    price_a: "٢٠",
+    price_b: "٥٠",
+    img: femina122,
+    mob_img: mobileFemina122,
+  },
+  {
+    brand: "Cailler",
+    name: "Femina 127 جرام",
+
+    product_description:
+      "حتى تكشف عن الخليط المغري من شوكولاتة الحليب الناعمة وكريمة البندق الذائبة Femina هكذا تبدو اللذة. أزل غلاف الفويل الشهير ل",
+    price_a: "٢٠",
+    price_b: "٥٠",
+    img: femina127,
+    mob_img: mobileFemina127,
+  },
+  {
+    brand: "Cailler",
+    name: "Femina 250 جرام",
+    product_description:
+      "حتى تكشف عن الخليط المغري من شوكولاتة الحليب الناعمة وكريمة البندق الذائبة Femina هكذا تبدو اللذة. أزل غلاف الفويل الشهير ل",
+    price_a: "٤٠",
+    price_b: "٨٠",
+    img: femina250,
+    mob_img: mobileFemina250,
+  },
+  {
+    brand: "Cailler",
+    name: "Femina 500 جرام",
+    product_description:
+      "حتى تكشف عن الخليط المغري من شوكولاتة الحليب الناعمة وكريمة البندق الذائبة Femina هكذا تبدو اللذة. أزل غلاف الفويل الشهير ل",
+    price_a: "٧٠",
+    price_b: "١٢٥",
+    img: femina500,
+    mob_img: mobileFemina500,
+  },
+  {
+    brand: "Cailler",
+    name: "Ambassador 122 جرام ",
+    product_description:
+      "المفضلة. سواءًا كهديةٍ أو حلوى، هي المدخل المثالي لعالم الشوكولاتة السويسرية cailler هذه تشكيلةٌ راقيةٌ من كريمة برالين اللوز الكلاسيكية، مجموعةٌ ناعمةٌ ومشبعةٌ من منتجات",
+    price_a: "٢٠",
+    price_b: "٥٠",
+    img: ambassador122,
+    mob_img: mobileAmbassador122,
+  },
+  {
+    brand: "Cailler",
+    name: "Ambassador 127 جرام ",
+    product_description:
+      "المفضلة. سواءًا كهديةٍ أو حلوى، هي المدخل المثالي لعالم الشوكولاتة السويسرية cailler هذه تشكيلةٌ راقيةٌ من كريمة برالين اللوز الكلاسيكية، مجموعةٌ ناعمةٌ ومشبعةٌ من منتجات",
+    price_a: "٢٠",
+    price_b: "٥٠",
+    img: ambassador127,
+    mob_img: mobileAmbassador127,
+  },
+  {
+    brand: "Cailler",
+    name: "Ambassador 250 جرام ",
+    product_description:
+      "المفضلة. سواءًا كهديةٍ أو حلوى، هي المدخل المثالي لعالم الشوكولاتة السويسرية cailler هذه تشكيلةٌ راقيةٌ من كريمة برالين اللوز الكلاسيكية، مجموعةٌ ناعمةٌ ومشبعةٌ من منتجات",
+    price_a: "٤٠",
+    price_b: "٨٠",
+    img: ambassador250,
+    mob_img: mobileAmbassador250,
+  },
+  {
+    brand: "Cailler",
+    name: "Ambassador 490 جرام ",
+    product_description:
+      "المفضلة. سواءًا كهديةٍ أو حلوى، هي المدخل المثالي لعالم الشوكولاتة السويسرية cailler هذه تشكيلةٌ راقيةٌ من كريمة برالين اللوز الكلاسيكية، مجموعةٌ ناعمةٌ ومشبعةٌ من منتجات",
+    price_a: "٧٠",
+    price_b: "١٢٥",
+    img: ambassador490,
+    mob_img: mobileAmbassador490,
+  },
+];
+
+export default cailler;
