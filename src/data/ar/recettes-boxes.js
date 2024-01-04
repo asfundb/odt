3a2d62401c973a8de9a3c7a7d7767b234400c587
asfundb <muhammedbagha5@gmail.com>
2024-01-04 14:15:19 +0400

    ksa recettes boxes

---
+++ src/data/ar/recettes-boxes.js
@@ -0,0 +1,77 @@
+import bigMilkAssorted from "../../../public/recettes-boxes/Desktop/big-milk-assorted.svg";
+import mobileBigMilkAssorted from "../../../public/recettes-boxes/Mobile/big-milk-assorted.png";
+import bigAssorted from "../../../public/recettes-boxes/Desktop/big-assorted.svg";
+import mobileBigAssorted from "../../../public/recettes-boxes/Mobile/big-assorted.png";
+import smallAssorted from "../../../public/recettes-boxes/Desktop/small-assorted.svg";
+import mobileSmallAssorted from "../../../public/recettes-boxes/Mobile/small-assorted.png";
+import smallCocao from "../../../public/recettes-boxes/Desktop/small-cocao-beans.svg";
+import mobileSmallCocao from "../../../public/recettes-boxes/Mobile/small-cocao-beans.png";
+import smallHazelnut from "../../../public/recettes-boxes/Desktop/small-hazelnut.svg";
+import mobileSmallHazelnut from "../../../public/recettes-boxes/Mobile/small-hazelnut.png";
+import smallSaltedCaramel from "../../../public/recettes-boxes/Desktop/small-salted-caramel.svg";
+import mobileSmallSaltedCaramel from "../../../public/recettes-boxes/Mobile/small-salted-caramel.png";
+
+const recettesBoxes = [
+  {
+    brand: "Recettes De L'Atelier",
+    name: "قطع الشوكولاتة بالحليب 406 جرام",
+    product_description:
+      "ستجد خبرة الشوكولاتة الفريدة من نوعها كامنةً في هذه التشكيلة من القطع الثلاث المغطاة بحليب الشوكولاتة الحلو للذواقة: قطع البندق، حبوب الكاكاو والكاراميل المملح",
+    price_a: "٤٥",
+    price_b: "٦٠",
+    img: bigMilkAssorted,
+    mob_img: mobileBigMilkAssorted,
+  },
+  {
+    brand: "Recettes De L'Atelier",
+    name: "قطع الشوكولاتة 398 جرام",
+    product_description:
+      "خبرة الشوكولاتة الفريدة من نوعها الكامنة في هذه التشكيلة من القطع الأربع للذواقة: قطع البندق، الكاراميل المملح، حبوب الكاكاو وتوت العليق قوي النكهة",
+    price_a: "٤٥",
+    price_b: "٦٠",
+    img: bigAssorted,
+    mob_img: mobileBigAssorted,
+  },
+  {
+    brand: "Recettes De L'Atelier",
+    name: "قطع الشوكولاتة المتنوعة 265 جرام",
+    product_description:
+      "اللذيذة لدينا في هذا القالب التي تجمع بين وصفات البندق / الكاراميل / حبوب الكاكاو خاصتنا طريةٌ من الداخل، مقرمشةٌ من الخارج، وبقطعٍ فاكهةٍ مجففةٍ/حبوب الكاكاو من فوقها. مصنوعةٌ من شوكولاتة زبدة الكاكاو الخالصة.Les Recettes de l'Atelier اكتشف قطع",
+    price_a: "٣٢",
+    price_b: "٤٢",
+    img: smallAssorted,
+    mob_img: mobileSmallAssorted,
+  },
+  {
+    brand: "Recettes De L'Atelier",
+    name: "حبة الكاكاو 265 جرام",
+    product_description:
+      "اكتشف القطع اللذيذة المحشوة بجناش الكاكاو لدينا، بطبقةٍ خارجيةٍ من الشوكولاتة الداكنة مع رقائق حبوب الكاكاو المنثورة عليها. مصنوعةٌ من شوكولاتة زبدة الكاكاو الخالصة",
+    price_a: "٣٢",
+    price_b: "٤٢",
+    img: smallCocao,
+    mob_img: mobileSmallCocao,
+  },
+  {
+    brand: "Recettes De L'Atelier",
+    name: "قطع البندق 265 جرام",
+    product_description:
+      "اكتشف القطع اللذيذة لدينا بحشوةٍ كريميةٍ من برالين اللوز، طبقةٍ خارجيةٍ من شوكولاتة الحليب مع قطع البندق المقرمشة المنثورة عليها. مصنوعةٌ من شوكولاتة زبدة الكاكاو الخالصة",
+    price_a: "٣٢",
+    price_b: "٤٢",
+    img: smallHazelnut,
+    mob_img: mobileSmallHazelnut,
+  },
+  {
+    brand: "Recettes De L'Atelier",
+    name: "قطع الكاراميل المملحة 265 جرام",
+    product_description:
+      "اكتشف القطع اللذيذة لدينا بحشوةٍ ذائبةٍ وإضافاتٍ مقرمشةٍ. مصنوعةٌ من شوكولاتة زبدة الكاكاو الخالصة",
+    price_a: "٣٢",
+    price_b: "٤٢",
+    img: smallSaltedCaramel,
+    mob_img: mobileSmallSaltedCaramel,
+  },
+];
+
+export default recettesBoxes;
