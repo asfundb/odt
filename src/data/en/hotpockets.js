7924f31c76b58baa33dddd478a1093729c8eefe0
asfundb <muhammedbagha5@gmail.com>
2024-01-07 13:23:18 +0400

    stouffers & hotpockets

--- src/data/en/hotpockets.js
+++ src/data/en/hotpockets.js
@@ -1,51 +1,77 @@
+import fourCheese from "../../../public/Hotpocket/Desktop/four-cheese.svg";
+import buffalo from "../../../public/Hotpocket/Desktop/buffalo.svg";
+import greenChile from "../../../public/Hotpocket/Desktop/Green-chile.svg";
+import phillySteak from "../../../public/Hotpocket/Desktop/Philly-steak.svg";
+import steakCheddar from "../../../public/Hotpocket/Desktop/Steak-and-cheddar.svg";
+import steakNacho from "../../../public/Hotpocket/Desktop/Steak-and-nachos.svg";
+
+import fourCheeseMobile from "../../../public/Hotpocket/Mobile/four-cheese.jpg";
+import buffaloMobile from "../../../public/Hotpocket/Mobile/buffalo.jpg";
+import greenChileMobile from "../../../public/Hotpocket/Mobile/Green-chile.jpg";
+import phillySteakMobile from "../../../public/Hotpocket/Mobile/Philly-steak.jpg";
+import steakCheddarMobile from "../../../public/Hotpocket/Mobile/Steak-and-cheddar.jpg";
+import steakNachoMobile from "../../../public/Hotpocket/Mobile/Steak-and-nachos.jpg";
+
 const hotpockets = [
   {
     brand: "Hot Pockets",
-    name: "FOUR CHEESE PIZZA",
+    name: "Four Cheese Pizza (5pcs)",
     product_description:
-      "Lorum Ipsum Dolomere akjsbd ajsdgiaj jashdijbha ajhskdabs",
-    price_a: 30,
-    price_b: 20,
+      "Melty parmesan, cheddar, reduced-fat provolone, & mozzarella cheeses all wrapped up in one delicious pocket. A hearty snack made with loads of real cheese that's ready in minutes. 5 Hot Pockets® sandwiches per box",
+    price_a: 50,
+    price_b: 56,
+    img: fourCheese,
+    mob_img: fourCheeseMobile,
   },
   {
     brand: "Hot Pockets",
-    name: "STEAK & CHEDDAR",
+    name: "Steak & Cheddar (5pcs)",
     product_description:
-      "Lorum Ipsum Dolomere akjsbd ajsdgiaj jashdijbha ajhskdabs",
-    price_a: 30,
-    price_b: 20,
+      "Tender, sliced steak with cheddar cheese and sauce all wrapped up in one delicious pocket. A hearty snack made with loads of premium meat and cheese that's ready in minutes. 5 Hot Pockets® sandwiches per box",
+    price_a: 50,
+    price_b: 56,
+    img: steakCheddar,
+    mob_img: steakCheddarMobile,
   },
   {
     brand: "Hot Pockets",
-    name: "PHILLY STEAK & CHEESE",
+    name: "Philly Steak & Cheese (5pcs)",
     product_description:
-      "Lorum Ipsum Dolomere akjsbd ajsdgiaj jashdijbha ajhskdabs",
-    price_a: 30,
-    price_b: 20,
+      "Savory beef, reduced-fat mozzarella cheese, peppers, onions, and sauce all wrapped up in one delicious pocket. A hearty snack made with loads of premium meat and cheese that's ready in minutes. 5 Hot Pockets® sandwiches per box",
+    price_a: 50,
+    price_b: 56,
+    img: phillySteak,
+    mob_img: phillySteakMobile,
   },
   {
     brand: "Hot Pockets",
-    name: "STEAK NACHO",
+    name: "Steak Nacho (4pcs)",
     product_description:
-      "Lorum Ipsum Dolomere akjsbd ajsdgiaj jashdijbha ajhskdabs",
+      "Unapologetically spicy steak, jalapeños and cheese sauce all wrapped up in one delicious pocket. 50% Larger* as compared to a traditional 4.5oz HOT POCKETS sandwich. 4 Hot Pockets® frozen sandwiches per box",
     price_a: 30,
     price_b: 20,
+    img: steakNacho,
+    mob_img: steakNachoMobile,
   },
   {
     brand: "Hot Pockets",
-    name: "BUFFALO STYLE CHICKEN",
+    name: "Buffalo Style Chicken (4pcs)",
     product_description:
-      "Lorum Ipsum Dolomere akjsbd ajsdgiaj jashdijbha ajhskdabs",
+      "Loads of white meat chicken and melty cheddar cheese wrapped in a buffalo blasted crust with a spicy buffalo sauce 50% Larger* as compared to a traditional 4.5oz Hot Pockets® sandwich. 4 Hot Pockets® frozen sandwiches per box",
     price_a: 30,
     price_b: 20,
+    img: buffalo,
+    mob_img: buffaloMobile,
   },
   {
     brand: "Hot Pockets",
-    name: "GREEN CHILI CHEESESTEAK",
+    name: "Green Chile Cheesesteak (2pcs)",
     product_description:
-      "Lorum Ipsum Dolomere akjsbd ajsdgiaj jashdijbha ajhskdabs",
-    price_a: 30,
-    price_b: 20,
+      "Spicy & juicy cheesesteak, jalapeno peppers, cilantro and real reduced fat mozzarella cheese in a crispy seasoned crust Made with Hot Ones™ Los Calientes™ Verde hot sauce Experience the thrill of the Hot Ones™ challenge right from your home",
+    price_a: 22,
+    price_b: 28,
+    img: greenChile,
+    mob_img: greenChileMobile,
   },
 ];
 export default hotpockets;
---
+++ src/data/en/stouffers-snack.js
@@ -0,0 +1,40 @@
+import macAndCheeseDesktop from "../../../public/stouffers-snack/Desktop/mac-and-cheese.svg";
+import macAndCheeseMobile from "../../../public/stouffers-snack/Mobile/mac-and-cheese.jpg";
+import tatersDesktop from "../../../public/stouffers-snack/Desktop/loaded-tots.svg";
+import tatersMobile from "../../../public/stouffers-snack/Mobile/loaded-tots.jpg";
+import nashvilleHotDesktop from "../../../public/stouffers-snack/Desktop/nashville-hot.svg";
+import nashvilleHotMobile from "../../../public/stouffers-snack/Mobile/nashville-hot.jpg";
+
+const stouffersSnacks = [
+  {
+    brand: "Stouffer's",
+    name: "Mac & Cheese Bites",
+    product_description:
+      "Crispy breading stuffed with macaroni and creamy cheddar cheese. Made with ingredients you can feel good about for a homemade snack or appetizer you will love. Freshly Made, Simply Frozen",
+    price_a: 36,
+    price_b: 48,
+    img: macAndCheeseDesktop,
+    mob_img: macAndCheeseMobile,
+  },
+  {
+    brand: "Stouffer's",
+    name: "Loaded Tater Tot Bites",
+    product_description:
+      "Crispy breading stuffed with potato, cheddar cheese, chives, sour cream & bacon. Made with ingredients you can feel good about for a homemade tasty snack or appetizer you will love. Try in Your Air Fryer",
+    price_a: 36,
+    price_b: 48,
+    img: tatersDesktop,
+    mob_img: tatersMobile,
+  },
+  {
+    brand: "Stouffer's",
+    name: "Nashville Hot Style Mac Bites",
+    product_description:
+      "Crispy, Nashville hot style breading stuffed with macaroni and creamy cheddar cheese. Made with ingredients you can feel good about for a homemade tasty snack or appetizer you will love. Freshly Made, Simply Frozen",
+    price_a: 36,
+    price_b: 48,
+    img: nashvilleHotDesktop,
+    mob_img: nashvilleHotMobile,
+  },
+];
+export default stouffersSnacks;
